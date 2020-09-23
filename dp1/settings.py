02df6fc10e1d737468ddf48d0d4cfc132281e04b
unknown <sureshbabupindra@gmail.com>
2020-09-23 14:50:37 +0530

    initial commit

--- dp1/settings.py
+++ dp1/settings.py
@@ -51,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
+]
 
 ROOT_URLCONF = 'dp1.urls'
 
